ece2add4d4b21053eb0d9932798c457007c6c1cd
AdeBeeY <adewusiadebayo@gmail.com>
2023-11-20 12:35:27 +0100

    add class to buttons and select all in js

--- script.js
+++ script.js
@@ -21,3 +21,22 @@ let secondNumber = 0;
 function operate(numb1, operator, numb2) {
   return operator(numb1, numb2);
 };
+
+const displayBtn = document.querySelector('.js-display');
+const btn1Element = document.querySelector('.btn-1');
+const btn2Element = document.querySelector('.btn-2');
+const btn3Element = document.querySelector('.btn-3');
+const btn4Element = document.querySelector('.btn-4');
+const btn5Element = document.querySelector('.btn-5');
+const btn6Element = document.querySelector('.btn-6');
+const btn7Element = document.querySelector('.btn-7');
+const btn8Element = document.querySelector('.btn-8');
+const btn9Element = document.querySelector('.btn-9');
+const btn0Element = document.querySelector('.btn-0');
+const divideBtnElement = document.querySelector('.divide-btn');
+const multiplyBtnElement = document.querySelector('.multiply-btn');
+const subtractBtnElement = document.querySelector('.subtract-btn');
+const operateBtnElement = document.querySelector('.operate-btn');
+const addBtnElement = document.querySelector('.add-btn');
+const clearBtnElement = document.querySelector('.clear-btn');
+
